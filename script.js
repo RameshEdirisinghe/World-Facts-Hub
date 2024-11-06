da1d2f0fc5361cfd89889fb88a6807860ef849f5
Ramesh Edirisinghe <e2145434@bit.uom.lk>
2024-11-06 16:57:44 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+loadItems();
+
+async function loadItems() {
+    
+    let res = await fetch("https://restcountries.com/v3.1/all");
+    let items = await res.json();
+    let body = "";
+    items.forEach(element => {
+        console.log(element);
+        body+=`
+            <div class="col m-5">
+                <div class="card shadow-sm">
+                  <img src=${element.flags.png} alt="">
+                  <div class="card-body">
+                    <h3 class="card-text fw-bold">${element.name.common}</h3>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      </div>
+                      <small class="text-body-secondary">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        `;        
+    });
+
+    console.log(body);
+
+    document.getElementById("card").innerHTML=body;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function search() {
     let searchCountry = document.getElementById("Search").value;
     let card = document.getElementById("card");
